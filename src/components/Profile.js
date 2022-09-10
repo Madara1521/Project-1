3b8a04582cfac9fb7fc77719b15301f73fb26069
Madara1521 <gusevnikita953@gmail.com>
2022-09-10 16:38:45 +0300

    first cpmmit

---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return  <div className={'content'}>
+        <div>
+            <img src={'https://kartinkin.net/uploads/posts/2021-07/1625616711_9-kartinkin-com-p-arti-naruto-konokha-art-krasivo-10.jpg'} />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
