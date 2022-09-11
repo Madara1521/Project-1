846f06bd01bb9d1c3edf963d167801b11f452ae3
Madara1521 <gusevnikita953@gmail.com>
2022-09-11 15:54:50 +0300

    css-modules

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
+import s from './Navbar.module.css'
+
 const Navbar = () => {
-    return <nav className={'nav'}>
-        <div>
-            <a>Profile</a>
+    return <nav className={s.nav}>
+        <div className={s.item}>
+            <a className={s.item}>Profile</a>
         </div>
-        <div>
+        <div className={`${s.item} ${s.active}`}>
             <a>Messages</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>News</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Music</a>
         </div>
-        <div>Settings</div>
+        <div className={s.item}>Settings</div>
     </nav>
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,7 @@
+import s from './Profile.module.css'
+
 const Profile = () => {
-    return  <div className={'content'}>
+    return  <div className={s.content}>
         <div>
             <img src={'https://kartinkin.net/uploads/posts/2021-07/1625616711_9-kartinkin-com-p-arti-naruto-konokha-art-krasivo-10.jpg'} />
         </div>
@@ -11,12 +13,14 @@ const Profile = () => {
             <div>
                 new post
             </div>
-            <div>
+            <div className={s.posts}>
+            <div className={s.item}>
                 post 1
             </div>
-            <div>
+            <div className={s.item}>
                 post 2
             </div>
+        </div>
         </div>
     </div>
 }
