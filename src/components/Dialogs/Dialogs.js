c9ff7ff127a93dd53932d1e9351dae765a2b5065
Madara1521 <gusevnikita953@gmail.com>
2022-09-13 21:04:07 +0300

    verstka

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,8 +2,29 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                 Nikita
+                </div>
+                <div className={s.dialog}>
+                 Slavik
+                </div>
+                <div className={s.dialog}>
+                 Stas
+                </div>
+                <div className={s.dialog}>
+                 Vlad
+                </div>
+                <div className={s.dialog}>
+                 Andreu
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>How is your neki4</div>
+                <div className={s.message}>yo</div>
+            </div>
         </div>
     )
 }
