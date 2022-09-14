0d7e5cd51ecf0754f9605a3e8d6f8e73471d6dc7
Madara1521 <gusevnikita953@gmail.com>
2022-09-14 16:12:43 +0300

    UI-BLL

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,15 +7,19 @@ const Navbar = () => {
         <div>
             <NavLink to={'/profile'} className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
         </div>
-        <div className={`${s.item} ${s.active}`}>
+        <br/>
+        <div>
             <NavLink to={'/dialogs'} className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink>
         </div>
+        <br/>
         <div >
             <NavLink to={'/news'} className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
         </div>
+        <br/>
         <div>
             <NavLink to={'/music'} className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
         </div>
+        <br/>
         <div>
             <NavLink to={'/settings'} className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
         </div>
