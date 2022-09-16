eab57c8895217099bae64814a2a90899fc4d5d41
Madara1521 <gusevnikita953@gmail.com>
2022-09-16 17:35:46 +0300

    onClick

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,12 +1,20 @@
 import s from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogsItem'
 import Message from "./Message/message";
+import React from "react";
 
 const Dialogs = (props) => {
 
     let dialogsElements = props.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>)
 
     let messagesElements = props.messages.map(m => <Message message={m.message}/>)
+
+    let newMessageElement = React.createRef()
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value
+        alert(text)
+    }
 
     return (
         <div className={s.dialogs}>
@@ -15,6 +23,12 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <div>
+                <textarea ref={newMessageElement}></textarea>
+                    <div>
+                    <button onClick={addMessage}>Add message</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
