f867ebae7b42acfd2a27c3a3deda4e46037db432
Madara1521 <gusevnikita953@gmail.com>
2022-09-21 20:07:10 +0300

    Redux install

--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -1,8 +1,23 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Nikita'},
+        {id: 2, name: 'Sla'},
+        {id: 3, name: 'Gus'},
+        {id: 4, name: 'Vlad'},
+        {id: 5, name: 'Andreu'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How is your nek?'},
+        {id: 3, message: 'Yo'}
+    ],
+    newMessageBody: ''
+}
 
-const dialogsReducer = (state, action) => {
+const dialogsReducer = (state = initialState, action) => {
 switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
         state.newMessageBody = action.body
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import store from "./Redux/state";
+import store from "./Redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -22,7 +22,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState())
 
-store.subscribe(rerenderEntireTree)
+store.subscribe( () => {
+    let state = store.getState()
+    rerenderEntireTree(state)
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
