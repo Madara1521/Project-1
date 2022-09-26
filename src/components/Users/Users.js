de9f7560248082baa733f88b688e3e07ad8d832d
Madara1521 <gusevnikita953@gmail.com>
2022-09-26 23:53:06 +0300

    life cycle methods

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,11 +4,12 @@ import styles from './users.module.css'
 import * as axios from "axios";
 
 class Users extends React.Component {
-constructor(props) {
-    super(props);
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items)
-        })
+
+componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+        this.props.setUsers(response.data.items)
+    })
 }
 
     render() {
