a72a347d184e6eac3ccd878bc4e815b4d91b3010
Madara1521 <gusevnikita953@gmail.com>
2022-10-03 23:53:24 +0300

    login

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import s from './Header.module.css'
 import SharinganIcon from './sharingan.png'
+import { NavLink } from 'react-router-dom'
 
-const Header = () => {
-    return (
-      <header className={s.header}>
-        <img src={SharinganIcon}/>
-      </header>
-    )
+const Header = (props) => {
+  return (
+    <header className={s.header}>
+      <img src={SharinganIcon}/>
+
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login :
+          <NavLink to={'/login'}>Login</NavLink>}
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
