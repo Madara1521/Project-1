04293b0cf51c30d4f2a19a797adf0f17cccae6b8
Madara1521 <gusevnikita953@gmail.com>
2022-10-24 20:30:02 +0300

    initialized

--- src/App.js
+++ src/App.js
@@ -6,31 +6,51 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from './components/Profile/ProfileContainer'
+import ProfileContainer, {withRouter} from './components/Profile/ProfileContainer'
 import HeaderContainer from './components/Header/HeaderContainer'
 import Login from './components/Login/Login'
+import {Component} from "react";
+import {connect} from "react-redux";
+import {compose} from "redux";
+import {initializeApp} from "./Redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
 
-const App = (props) => {
+class App extends Component {
+  componentDidMount() {
+    this.props.initializeApp()
+  }
+
+  render() {
+    if(!this.props.initialized) {
+      return <Preloader/>
+    }
+
     return (
-        <div className={'app-wrapper'}>
-            <HeaderContainer/>
-            <Navbar/>
-            <div className={'app-wrapper-content'}>
-                <Routes>
-                    <Route path={'/dialogs/*'} element={<DialogsContainer/>}/>
-                    <Route path="/profile" element={<ProfileContainer/>}>
-                        <Route path=":userId" element={<ProfileContainer/>}/>
-                    </Route>
-                    <Route path={'/news/*'} element={<News/>}/>
-                    <Route path={'/music/*'} element={<Music/>}/>
-                    <Route path={'/settings/*'} element={<Settings/>}/>
-                    <Route path={'/users/*'} element={<UsersContainer/>}/>
-                    <Route path={'/login/*'} element={<Login/>}/>
-                </Routes>
-            </div>
+      <div className={'app-wrapper'}>
+        <HeaderContainer/>
+        <Navbar/>
+        <div className={'app-wrapper-content'}>
+          <Routes>
+            <Route path={'/dialogs/*'} element={<DialogsContainer/>}/>
+            <Route path="/profile" element={<ProfileContainer/>}>
+              <Route path=":userId" element={<ProfileContainer/>}/>
+            </Route>
+            <Route path={'/news/*'} element={<News/>}/>
+            <Route path={'/music/*'} element={<Music/>}/>
+            <Route path={'/settings/*'} element={<Settings/>}/>
+            <Route path={'/users/*'} element={<UsersContainer/>}/>
+            <Route path={'/login/*'} element={<Login/>}/>
+          </Routes>
         </div>
+      </div>
     )
+  }
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized
+})
+
+export default compose(withRouter,
+  connect(mapStateToProps, {initializeApp}))(App)
--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -1,21 +1,22 @@
 import Profile from './Profile'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {getStatus, getUserProfile, updateStatus} from '../../Redux/profile-reducer'
 import React from 'react'
-import { useParams } from 'react-router-dom'
-import { compose } from 'redux'
+import {useParams} from 'react-router-dom'
+import {compose} from 'redux'
+import {withAuthRedirect} from "../Hoc/withAuthRedirect";
 
-export function withRouter (Children) {
+export function withRouter(Children) {
   return (props) => {
 
-    const match = { params: useParams() }
+    const match = {params: useParams()}
     return <Children {...props} match={match}/>
   }
 }
 
 class ProfileContainer extends React.Component {
 
-  componentDidMount () {
+  componentDidMount() {
     let userId = this.props.match.params.userId
     if (!userId) {
       userId = this.props.authorizedUserId
@@ -24,12 +25,12 @@ class ProfileContainer extends React.Component {
     this.props.getStatus(userId)
   }
 
-  render () {
+  render() {
     return (
       <Profile {...this.props}
                profile={this.props.profile}
                status={this.props.status}
-               updateStatus={this.props.updateStatus} />
+               updateStatus={this.props.updateStatus}/>
     )
   }
 }
@@ -43,10 +44,10 @@ let mapStateToProps = (state) => ({
 })
 
 
-export default compose(
-  connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
+export default compose(withAuthRedirect,
+  connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
   withRouter
-) (ProfileContainer)
+)(ProfileContainer)
 
 
 
