910c4702771d8e4e32130a01d7858a430318d363
Madara1521 <gusevnikita953@gmail.com>
2022-11-15 17:48:33 +0200

    profile update v4

--- src/components/Profile/ProfileInfo/ProfileDataForm.js
+++ src/components/Profile/ProfileInfo/ProfileDataForm.js
@@ -2,10 +2,15 @@ import React from "react";
 import s from './ProfileInfo.module.css'
 import {createField, Input, Textarea} from "../../common/FormsControls/FormsControls";
 import {reduxForm} from "redux-form";
+import style from "../../common/FormsControls/FormsControls.module.css";
 
-const ProfileDataForm = ({handleSubmit, profile}) => {
+const ProfileDataForm = ({handleSubmit, profile, error}) => {
   return <form onSubmit={handleSubmit}>
-      <button>save</button>
+     <div><button>save</button></div>
+    {error && <div className={style.formSummaryError}>
+      {error}
+    </div>
+    }
     <div>
       <b>Full name</b>: {createField('Full name', 'fullName', [], Input)}
     </div>
@@ -22,7 +27,7 @@ const ProfileDataForm = ({handleSubmit, profile}) => {
     </div>
     <div>
       <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
-      return <div className={s.contact}>
+      return <div key={key} className={s.contact}>
       <b>{key}: {createField(key, 'contacts.' + key, [], Input, )}
       </b>
       </div>
